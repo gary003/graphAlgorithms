f4f07c4581b1dfa93ad97b338a6fb5eb0edae73b
gary003 <gary.johnson003@gmail.com>
2021-09-22 23:53:39 +0200

    connected component done

---
+++ connectedComponentsCount.js
@@ -0,0 +1,54 @@
+const gph = {
+  a: ["c", "b"],
+  b: ["d"],
+  c: ["e"],
+  d: ["f"],
+  e: [],
+  f: [],
+  g: ["h"],
+  h: ["g"]
+}
+
+const connectedComponentsCount = (graph, visited = new Set()) => {
+  let composentCount = 0
+
+  for (let node in graph) {
+    if (visited.has(String(node))) continue
+    composentCount += 1
+    exploreRecursive(graph, node, visited)
+  }
+
+  return composentCount
+}
+
+const exploreRecursive = (graph, source, visited) => {
+  visited.add(String(source))
+  for (let neighbor of graph[source]) {
+    if (visited.has(String(neighbor))) continue
+    exploreRecursive(graph, neighbor, visited)
+  }
+}
+
+const exploreIterative = (graph, source, visited) => {
+  const queue = [source]
+  while (queue.length > 0) {
+    const currentNode = queue.shift()
+    visited.add(String(currentNode))
+    const unvisitedNeighbors = graph[currentNode].filter((neighbor) => !visited.has(neighbor))
+    queue.push(...unvisitedNeighbors)
+  }
+}
+
+console.log(
+  connectedComponentsCount(gph),
+  connectedComponentsCount({
+    3: [],
+    4: [6],
+    6: [4, 5, 7, 8],
+    8: [6],
+    7: [6],
+    5: [6],
+    1: [2],
+    2: [1]
+  })
+)
