d5feaea29c5196e719c2f25c6ef8e2bca3cfd600
gary003 <gary.johnson003@gmail.com>
2021-11-24 01:38:28 +0100

    longestPath finished

--- structy/longestPath.js
+++ structy/longestPath.js
@@ -20,62 +20,55 @@ const graphFromEList = (e_list) => {
   return graph
 }
 
-const nodesPath = (graph) => {
-  const nodePathLength = {}
-
-  for (let node of Object.keys(graph)) {
-    nodePathLength[node] = 0
-  }
-
-  return nodePathLength
-}
-
-const longestPath = (edges, nodeStart, nodeEnd, visited = new Set()) => {
+const longestPath = (edges, nodeStart, nodeEnd) => {
   const graph = graphFromEList(edges)
-  const allNodes = nodesPath(graph)
+  let pathsFound = []
 
-  console.log({ graph })
-
-  let stack = [[nodeStart, 0]]
+  let stack = [nodeStart]
 
   while (stack.length) {
-    const [currentNode, pathLength] = stack.pop()
+    const path = stack.pop()
 
-    console.log(graph[currentNode])
+    // console.log(path)
 
-    const validNeighbors = graph[currentNode].filter((neighbor) => {
-      if (allNodes[neighbor] < pathLength + 1) {
-        return true
-      }
-      return false
-    })
+    const pathArr = path.split(".")
+    const lastNode = pathArr[pathArr.length - 1]
 
-    for (let n of validNeighbors) {
-      stack.push([n, pathLength + 1])
-      allNodes[n] += 1
+    if (lastNode === nodeEnd) {
+      if (pathsFound.length === 0) pathsFound = [path]
+      else if (pathsFound[0].length < path.length) pathsFound = [path]
+      else if (pathsFound[0].length === path.length) pathsFound = [...pathsFound, path]
+
+      continue
     }
 
-    // console.log(validNeighbors, stack, allNodes)
+    // console.log(lastNode, graph[lastNode])
+
+    const validNeighbors = graph[lastNode].filter((n) => !pathArr.includes(n))
+
+    // console.log(validNeighbors)
+
+    const newPaths = validNeighbors.map((neighbor) => path + "." + neighbor)
+
+    // console.log(newPaths)
+
+    stack = [...stack, ...newPaths]
   }
 
-  return allNodes[nodeEnd]
+  return pathsFound
 }
 
-console.log(
-  longestPath(
-    [
-      ["a", "c"],
-      ["a", "b"],
-      ["c", "b"],
-      ["c", "d"],
-      ["b", "d"],
-      ["b", "e"],
-      ["e", "d"],
-      ["d", "g"],
-      ["f", "e"],
-      ["g", "f"]
-    ],
-    "a",
-    "e"
-  )
-)
+const arrTest = [
+  ["a", "c"],
+  ["a", "b"],
+  ["c", "b"],
+  ["c", "d"],
+  ["b", "d"],
+  ["b", "e"],
+  ["e", "d"],
+  ["d", "g"],
+  ["f", "e"],
+  ["g", "f"]
+]
+
+console.log(longestPath(arrTest, "a", "e"))
