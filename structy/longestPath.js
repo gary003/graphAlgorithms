d1e5368f4794e4a8905160c6fbe4b17d930ed9b6
gary003 <gary.johnson003@gmail.com>
2021-11-25 05:27:53 +0100

    updatelongest path

--- structy/longestPath.js
+++ structy/longestPath.js
@@ -1,7 +1,7 @@
 /*
-Write a function, shortestPath, 
+Write a function, longestPath, 
 that takes in an array of edges for an undirected graph and two nodes (nodeA, nodeB). 
-The function should return the length of the shortest path between A and B. 
+The function should return the length of the longest path between A and B. 
 Consider the length as the number of edges in the path, not the number of nodes. 
 If there is no path between A and B, then return -1.
 */
